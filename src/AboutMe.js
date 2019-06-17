e3dfb1a816c599fba6ffcf28d872d6d4d67e62cc
github username <mariacristina.simoes@gmail.com>
2019-06-16 20:52:29 -0400

    about mc

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -8,6 +8,7 @@ class AboutMe extends React.Component {
   render () {
       return (
         <div className="aboutMe">
+        <h2> About Maria Cristina </h2>
         <p> Maria Cristina is a full-stack web developer with experience in Ruby on Rails, JavaScript, and React and a passion for design. From experience in retail buying, e-commerce and marketing, she brings strong skills in analytics and consumer-behavior coupled with creativity, hard-work and a team-oriented mindset. She is driven to create high-performance products with a positive user experience, seeking to help others find their journey and destination on the web. </p>
         <p></p>
         </div>
