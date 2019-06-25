8659f967482a3572812eb85e8b0aa3cb4f10553b
github username <mariacristina.simoes@gmail.com>
2019-06-24 23:16:58 -0400

    navlinks style

--- src/MyInfo.js
+++ src/MyInfo.js
@@ -10,10 +10,12 @@ class MyInfo extends React.Component {
       return (
         <div className="navBar">
           <div className="myName"> <h1> maria cristina simoes </h1> </div>
-          <Router> <Link to="/">About Me</Link> </Router>
-          <Router> <Link to="/">Projects</Link> </Router>
-          <Router> <Link to="/">Skills</Link> </Router>
-          <Router> <Link to="/">Contact</Link> </Router>
+          <div className="myLinks">
+          <Router className="myLinks"> <Link to="/">About Me</Link> </Router>
+          <Router className="myLinks"> <Link to="/">Projects</Link> </Router>
+          <Router className="myLinks"> <Link to="/">Skills</Link> </Router>
+          <Router className="myLinks"> <Link to="/">Contact</Link> </Router>
+        </div>
         </div>
       )
     }
